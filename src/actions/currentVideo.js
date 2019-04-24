4bf3a69a4742a219374cad7201e5da54429f5925
Chadcramer <chadpcramer@gmail.com>
2019-04-24 12:21:58 -0500

    completed reducers

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,7 @@
+//should return an object
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  currentVideo: video,
 });
 
 export default changeVideo;
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -2,6 +2,20 @@ import Redux from 'redux';
 
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  //copy our state
+  //for every key in action object
+  //update matching coptied states key to action keys val
+  //return new copied state
+  let newState = {};
+  for (let key in state) {
+    newState[key] = state[key];
+  }
+
+  for (let key in action) {
+    newState[key] = action[key];
+  }
+
+  return newState;
 };
 
 export default currentVideoReducer;
